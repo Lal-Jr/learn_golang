a3e77644f8f339d64e049bec49443963c9c78dcf
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-11-12 16:15:08 +0530

    outputing value after logic execution

--- main.go
+++ main.go
@@ -4,18 +4,32 @@ import "fmt"
 
 func main() {
 	conferenceName := "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
     fmt.Printf("Welcome to our %v booking application\n", conferenceName)
     fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var userFirstName string
+	var userLastName string
+	var userEmail string
 	var userTickets int
 	
-	fmt.Scan(&userName)
-	userTickets = 2
+	fmt.Print("Enter First Name: ")
+	fmt.Scan(&userFirstName)
 
-	fmt.Printf("%v booked %v tickets", userName, userTickets)
+	fmt.Print("Enter Last Name: ")
+	fmt.Scan(&userLastName)
+
+	fmt.Print("Enter Email Address: ")
+	fmt.Scan(&userEmail)
+
+	fmt.Print("Enter No. of Tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets -= uint(userTickets);
+
+	fmt.Printf("Thanks %v %v for booking %v tickets. You will receive your confirmation at %v.\n", userFirstName, userLastName, userTickets, userEmail)
+	fmt.Printf("%v tickets remaining for %v", remainingTickets, conferenceName)
 }
